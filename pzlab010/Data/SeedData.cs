85d80d801f9aabf3e5eef780f4164bd0641a889b
bartlomiejmikolajczuk@gmail.com <bartlomiejmikolajczuk@gmail.com>
2023-06-26 20:57:17 +0200

    Add project files.

---
+++ pzlab010/Data/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pzlab010.Models;
+
+namespace pzlab010.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new pzlab010Context(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<pzlab010Context>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Rating = "A",
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Rating = "Y",
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Rating = "O",
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ pzlab010/Data/pzlab010Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using pzlab010.Models;
+
+namespace pzlab010.Data
+{
+    public class pzlab010Context : DbContext
+    {
+        public pzlab010Context (DbContextOptions<pzlab010Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<pzlab010.Models.Movie> Movie { get; set; } = default!;
+    }
+}
